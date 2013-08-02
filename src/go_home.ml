d160e827bbd847587b3a082354beff7a0cc80561
John Else <john.else@citrix.com>
2013-08-03 00:16:06 +0100

    Add database access
    Open and close the database, creating the directory if necessary.
    
    Signed-off-by: John Else <john.else@citrix.com>

--- src/go_home.ml
+++ src/go_home.ml
@@ -1,1 +1,31 @@
-let () = ()
+let relative_db_dir = ".config/go-home"
+let db_filename = "go-home.db"
+
+let ensure_dir_exists dir =
+  if Sys.file_exists dir then begin
+    if not (Sys.is_directory dir)
+    then failwith (Printf.sprintf "%s exists but is not a directory" dir)
+  end
+  else Unix.mkdir dir 0o755
+
+let with_db f =
+  let home_dir =
+    try Sys.getenv "HOME"
+    with Not_found -> failwith "Couldn't read home directory"
+  in
+  (* Make sure there's a directory for the database. *)
+  let db_dir = Filename.concat home_dir relative_db_dir in
+  ensure_dir_exists db_dir;
+  (* Open the database. *)
+  let db_path = Filename.concat db_dir db_filename in
+  let db = Sqlite3.db_open db_path in
+  try
+    let result = f db in
+    ignore (Sqlite3.db_close db);
+    result
+  with e ->
+    ignore (Sqlite3.db_close db);
+    raise e
+
+let () =
+  with_db (fun db -> ())
