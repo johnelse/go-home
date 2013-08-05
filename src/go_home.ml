dbad460681e7ceec131f978b8ac56ea180142af6
John Else <john.else@citrix.com>
2013-08-05 02:01:02 +0100

    Fix indentation
    Signed-off-by: John Else <john.else@citrix.com>

--- src/go_home.ml
+++ src/go_home.ml
@@ -15,20 +15,20 @@ module Db = struct
     | Sqlite3.Rc.OK -> ()
     | code -> raise (Bad_return_code code)
 
-let with_db db_path f =
-  (* Open the database. *)
-  let db_exists = Sys.file_exists db_path in
-  let db = Sqlite3.db_open db_path in
-  try
-    if not db_exists
-    then expect_ok (Sqlite3.exec db
-      "create table observations (year int, month int, day int, hour int, minute int)");
-    let result = f db in
-    ignore (Sqlite3.db_close db);
-    result
-  with e ->
-    ignore (Sqlite3.db_close db);
-    raise e
+  let with_db db_path f =
+    (* Open the database. *)
+    let db_exists = Sys.file_exists db_path in
+    let db = Sqlite3.db_open db_path in
+    try
+      if not db_exists
+      then expect_ok (Sqlite3.exec db
+        "create table observations (year int, month int, day int, hour int, minute int)");
+      let result = f db in
+      ignore (Sqlite3.db_close db);
+      result
+    with e ->
+      ignore (Sqlite3.db_close db);
+      raise e
 end
 
 let () =
